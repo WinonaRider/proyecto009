1a93bd4cfa7dfe9b4603ef5cb4df50b92c4285ef
unknown <marchioripedro1@gmail.com>
2023-04-12 21:58:23 -0300

    update all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import CoordenadaFlecha from './CoordenadaFlecha';
 
 function App() {
+
+  const [visible, setVisible] = useState(true)
+
+  function ocultar() {
+    setVisible(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {visible ? <CoordenadaFlecha /> : <p>Se oculto la coordenada</p>}
+      <button onClick={ocultar}>Ocultar</button>
     </div>
   );
 }
 
-export default App;
+export default App
